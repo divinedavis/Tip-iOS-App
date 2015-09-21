3e9ac9326e924e2f1449d08db8328f93b24fe2a7
divinedavis <divinejdavis@gmail.com>
2015-09-20 20:57:47 -0400

    Completed the tip calculator - 3 percentages to choose from -> 12%, 20%, 25% - Used a segment - Used a label - Used a text field - Used a button

--- Tip Calculator/Tip Calculator/ViewController.swift
+++ Tip Calculator/Tip Calculator/ViewController.swift
@@ -10,14 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dollarInput: UITextField!
+    @IBOutlet weak var taxButton: UISegmentedControl!
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBAction func calculateButton(sender: AnyObject) {
+        
+        //1. Get the total bill
+        let userInput = dollarInput.text as! NSString
+        let totalBill : Float = userInput.floatValue
+        
+        //2. Determine the tip rate
+        let index : Int = taxButton.selectedSegmentIndex
+        var tipRate : Float = 0.15
+        
+        switch (index) {
+        case 0:
+            tipRate = 0.15
+        case 1:
+            tipRate = 0.20
+        case 2:
+            tipRate = 0.25
+    break;
+
+  default:
+    break;
+}
+        //3. Calculate the tip
+        let tip = totalBill * tipRate
+        
+        //4. Display the tip
+        outputLabel.text = "$\(tip)"
+        
+        
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
